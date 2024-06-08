36057eb19452e86ce91c3172021d08e69f585473
RaySlash <stevemathewjoy@gmail.com>
2024-06-08 17:27:28 +1000

    prisma: add migrations and update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,31 +3,48 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.category.createMany({
+    data: [
+      {
+        name: "Work",
+      },
+      {
+        name: "Personal",
+      },
+    ],
+  });
+
   await prisma.note.createMany({
     data: [
       {
         title: "First Note",
         content: "This is the content of the first note.",
+        categoryId: 1,
       },
       {
         title: "Second Note",
         content: "This is the content of the second note.",
+        categoryId: 1,
       },
       {
         title: "Third Note",
         content: "This is the content of the third note.",
+        categoryId: 1,
       },
       {
         title: "Fourth Note",
         content: "This is the content of the fourth note.",
+        categoryId: 1,
       },
       {
         title: "Fifth Note",
         content: "This is the content of the fifth note.",
+        categoryId: 2,
       },
       {
         title: "Sixth Note",
         content: "This is the content of the sixth note.",
+        categoryId: 2,
       },
     ],
   });
